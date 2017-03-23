9e7c5f964bb3fc74a190842bddde2e2b9049db0a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-22 17:59:49 -0700

    add user details

--- server.py
+++ server.py
@@ -24,8 +24,6 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    session["logged_in"] = session.get('logged_in', False)
-
     return render_template("homepage.html")
 
 @app.route('/users')
@@ -41,6 +39,7 @@ def show_registration():
 
     return render_template("reg_form.html")
 
+
 @app.route('/process-registration', methods=['POST'])
 def process_registration():
     """Check the given email against the database"""
@@ -49,23 +48,26 @@ def process_registration():
     given_password = request.form.get("password")
     print given_email
 
-
     existing_user = User.query.filter(User.email==given_email).first()
 
     if existing_user:
-        flash("Great job! You already exist in our universe!")
+        flash("Great job! You already exist in our universe! Please log in!")
+        return redirect("/login-form")
 
     else:
         new_user = User(email=given_email, password=given_password)
+        user_id = new_user.user_id
 
         # We need to add to the session or it won't ever be stored
         db.session.add(new_user)
 
         # Once we're done, we should commit our work
         db.session.commit()
+        flash("user id = %s" % new_user.user_id)
         flash("Great job! Welcome to our universe! Get ready to pass judgement.")
+        session["user_id"] = user_id
 
-    return render_template("login_form.html")
+        return redirect("/")
 
 
 @app.route('/login-form')
@@ -86,13 +88,32 @@ def check_login():
 
     if valid_user:
         # add state of logged-in-ness to session
-        session["logged_in"] = True
+        session["user_id"] = valid_user.user_id
         # redirect to '/'
         flash("You are logged in now.")
         return redirect("/")
     else:
         flash("Username and password do not match. please try again")
         return redirect("/login-form")
+
+@app.route("/logged-out")
+def log_out():
+    """Log out."""
+
+    del session["user_id"] 
+    flash("you are so logged out.")
+    return redirect("/")
+
+
+@app.route("/<user_id>-details")
+def show_user_details(user_id):
+    """Shows each user's details when specific user clicked on.
+    """
+    user_object = User.query.get(user_id)
+    ratings = user_object.ratings
+    movies = [rating.movie.title for rating in ratings]
+
+    return render_template("user_details.html", age=user_object.age, zipcode=user_object.zipcode, movies=movies)
 
 
 if __name__ == "__main__":
